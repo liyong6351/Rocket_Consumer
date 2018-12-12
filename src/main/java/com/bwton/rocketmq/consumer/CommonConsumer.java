36518192ba06a9dd49f6035a8b964ccb5faf41b4
mac <mac@macdeMacBook-Pro.local>
2018-12-12 11:52:57 +0800

    添加gitignore

---
+++ src/main/java/com/bwton/rocketmq/consumer/CommonConsumer.java
@@ -0,0 +1,11 @@
+package com.bwton.rocketmq.consumer;
+
+/**
+ * @ClassName com.bwton.rocketmq.consumer.CommonComsumer
+ * @Description 无序消息的消费
+ * @Author liyong
+ * @Date 2018-12-12 11:10
+ * @Version 0.0.1
+ **/
+public class CommonConsumer {
+}
---
+++ src/main/java/com/bwton/rocketmq/consumer/ConsumerExecutor.java
@@ -0,0 +1,11 @@
+package com.bwton.rocketmq.consumer;
+
+/**
+ * @ClassName com.bwton.rocketmq.consumer.ConsumerExecutor
+ * @Description TODO
+ * @Author liyong
+ * @Date 2018-12-12 11:19
+ * @Version 0.0.1
+ **/
+public class ConsumerExecutor {
+}
